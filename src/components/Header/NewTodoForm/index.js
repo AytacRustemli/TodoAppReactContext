df084158c45819a9c2bf08e333274c61a9d33e2a
Aytacrustemli <aytacrustmli93@gmail.com>
2023-01-08 00:11:14 +0400

    08.01.2023

---
+++ src/components/Header/NewTodoForm/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Formik, Field, Form } from 'formik';
+import validationSchema from './validations';
+import { useTodo } from "../../../contexts/TodoContext";
+import { v4 as uuidv4} from 'uuid';
+
+function NewTodoForm() {
+  const {addTodo} = useTodo();
+
+  return (
+    <Formik
+      initialValues={{
+        text: "",
+      }}
+      onSubmit={(values, bag) => {
+        console.log(values);
+
+        addTodo(values.text)
+        
+        bag.resetForm();
+      }}
+      validationSchema = {validationSchema}
+    >
+
+    <Form>
+      <Field 
+        className="new-todo"
+        placeholder="What needs to be done?"
+        autoFocus
+        id="text" 
+        name="text"
+        />
+    </Form>
+
+    </Formik>
+  );
+}
+
+export default NewTodoForm;
